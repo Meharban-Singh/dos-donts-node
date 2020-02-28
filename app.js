4b38c73922fa177c0e7abbc9f765b390104b1fb6
Meharban-Singh <meharbansingh10.ms@gmail.com>
2020-02-28 01:35:03 -0600

    initial commit

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.engine('handlebars', exhbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 80);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     res.render("index");
 });
